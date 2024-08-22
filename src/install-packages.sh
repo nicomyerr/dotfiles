59005b72d35fa6764a438b241df034beba3c0d92
nicomyerr <mail@nicomeyer.me>
2024-08-23 00:09:33 +0200

    fix: refactored install packages script

--- src/install-packages.sh
+++ src/install-packages.sh
@@ -1,10 +1,14 @@
 PACKAGEMANAGERS=$(yq packages.yaml | grep -v '-' | tr -d ':')
 
+install_packages() {
+  while read PACKAGE; do
+    echo "sudo ${PACKAGEMANAGER} -S ${PACKAGE}"
+  done < <(echo $1)
+}
+
 while read PACKAGEMANAGER; do
   echo "Installing packages with '${PACKAGEMANAGER}'"
   PACKAGES=$(yq .${PACKAGEMANAGER} packages.yaml | sed 's|[- "]||g')
-  while read PACKAGE; do
-    echo "sudo ${PACKAGEMANAGER} -S ${PACKAGE}"
-  done < <(echo "${PACKAGES}")
+  install_packages ${PACKAGES}
   echo "Finished installing '${PACKAGEMANAGER}' packages"
 done < <(echo "${PACKAGEMANAGERS}")
