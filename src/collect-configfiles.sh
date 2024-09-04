7db07c92d7a72cd7c8e002c7ab746a1af10ae527
nicomyerr <mail@nicomeyer.me>
2024-09-04 22:18:01 +0200

    feat: added script for collecting configfiles

---
+++ src/collect-configfiles.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function get_full_path () {
+  if [ ! -z $1 ]; then
+    echo "$HOME/$path"
+  else
+    echo $HOME
+  fi
+}
+
+function copy_configfiles() {
+  config_files=$(yq .config-files system.yaml | sed 's|[- ]||g')
+
+  while read config_file; do
+    app=$(echo ${config_file} | cut -d ":" -f 1)
+    path=$(echo ${config_file} | cut -d ":" -f 2 | sed 's|["]||g')
+
+    full_path=$(get_full_path "$path")
+
+    if [ ! -z $path ]; then
+      echo "Copying configfiles for '$app' to repo"
+      cp -r $full_path/. config/$app/
+    fi
+
+  done < <(echo "${config_files}")
+}
+
+copy_configfiles
