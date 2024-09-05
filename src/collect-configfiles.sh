792177d216f5b3b019bfcd38900caccfc53e64c2
nicomyerr <mail@nicomeyer.me>
2024-09-05 23:05:43 +0200

    fix: fixed path for configfiles in home directory

--- src/collect-configfiles.sh
+++ src/collect-configfiles.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-function get_full_path () {
-  if [ ! -z $1 ]; then
-    echo "$HOME/$path"
-  else
-    echo $HOME
-  fi
-}
-
 function collec_configfiles() {
   config_files=$(yq .config-files system.yaml | sed 's|[- ]||g')
 
@@ -15,13 +7,17 @@ function collec_configfiles() {
     app=$(echo ${config_file} | cut -d ":" -f 1)
     path=$(echo ${config_file} | cut -d ":" -f 2 | sed 's|["]||g')
 
-    full_path=$(get_full_path "$path")
+    full_path="$HOME/$path"
 
-    if [ ! -z $path ]; then
+    echo "app: $app path: $full_path"
+
+    if [[ $path =~ ".config" ]]; then
       echo "Copying configfiles for '$app' to repo"
       cp -r $full_path/. config/$app/
+    else
+      echo "Copying configfile for '$app' to repo"
+      cp -r $full_path config/$app/
     fi
-
   done < <(echo "${config_files}")
 }
 
